33d485a2526aba9463426710597c0696ae065707
Matt Hotovy <mdhotovy@gmail.com>
2022-11-08 11:42:43 -0600

    Added in Accuweather api call

--- core.py
+++ core.py
@@ -1,1 +1,3 @@
 # This is the core file which will run our project
+# here is the curl command for the Accuweather api current conditions
+# curl -X GET "http://dataservice.accuweather.com/currentconditions/v1/349291?apikey=d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3&details=true"
