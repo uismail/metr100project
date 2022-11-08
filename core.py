3c7ecbb76b22e759356960eacc01f410c3a82c30
Matt Hotovy <mdhotovy@gmail.com>
2022-11-08 12:32:12 -0600

    Update core.py

--- core.py
+++ core.py
@@ -3,12 +3,7 @@
 # curl -X GET "http://dataservice.accuweather.com/currentconditions/v1/349291?apikey=d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3&details=true"
 import requests
 import json
+import functions
 
-
-# display current conditions based on key
-def get_conditions(key):
-    conditions_url = 'https://dataservice.accuweather.com/currentconditions/v1/' \
-                     f'?apikey={api}'.format(key)
-    response = requests.get(conditions_url)
-    json_version = response.json()
-    print("Current Conditions: {}".format(json_version[0].get('WeatherText')))
+# get the current conditions for Omaha: 349291 is the location key for Omaha
+response = functions.get_conditions(349291)
