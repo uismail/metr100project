12fe2632f06818691812e985c54704e72ca3e271
Matt Hotovy <mdhotovy@gmail.com>
2022-11-08 11:49:39 -0600

    Update core.py

--- core.py
+++ core.py
@@ -1,3 +1,14 @@
 # This is the core file which will run our project
 # here is the curl command for the Accuweather api current conditions
 # curl -X GET "http://dataservice.accuweather.com/currentconditions/v1/349291?apikey=d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3&details=true"
+import requests
+import json
+
+
+# display current conditions based on key
+def get_conditions(key):
+    conditions_url = 'https://dataservice.accuweather.com/currentconditions/v1/' \
+                     f'?apikey={api}'.format(key)
+    response = requests.get(conditions_url)
+    json_version = response.json()
+    print("Current Conditions: {}".format(json_version[0].get('WeatherText')))
