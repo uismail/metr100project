f5f8275f2b576c46003ff570cb9492932c5bc275
uismail <69741155+uismail@users.noreply.github.com>
2022-12-05 18:37:35 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@
 # curl -X GET "http://dataservice.accuweather.com/currentconditions/v1/349291?apikey=d1w8pFazvk8uKC7AzmWAxWCNP1mtEZK3&details=true"
 import requests
 import json
+import pandas as pd
 
 # display current conditions based on key
 def get_conditions(key):
@@ -12,3 +13,21 @@ def get_conditions(key):
     print("Current Conditions: {}".format(json_version[0].get('WeatherText')))
     return json_version
     
+def print_HistoryData():
+    pd.set_option('display.max_rows', None)
+    df = pd.read_csv (r'OmahaWeatherHistory copy 2.csv')
+    print("Eppley Airfield Historic Weather Data\n")
+    print("Cordinates:(41.301475,-95.894521) \n")
+    df.pop('dt') 
+    df.pop('lat')
+    df.pop('lon')
+    df.pop('city_name')
+    df.pop('sea_level')
+    df.pop('grnd_level')
+    df.pop('timezone')
+    df.pop('weather_icon')
+    df.pop('weather_description')
+    df.pop('wind_gust')
+    df.pop('weather_id')
+    df.pop('clouds_all')
+    print(df)
